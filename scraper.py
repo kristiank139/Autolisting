3614cbf14cd5a9bd77ae014831bccb05572eddff
kristiank139 <105385905+kristiank139@users.noreply.github.com>
2025-06-24 14:40:06 +0300

    Fixes + new features

--- scraper.py
+++ scraper.py
@@ -28,17 +28,18 @@ def send_notification_email(cars_dict):
         html_body += f"""
         <li style="margin-bottom: 15px;">
             <strong>{car['nimi']}</strong><br>
-            Year: {car['aasta']}<br>
-            Price: €{car['hind']}<br>
-            Mileage: {car['mileage']} km<br>
-            Fuel: {car['kütus']}<br>
-            Transmission: {car['kast']}<br>
-            Drive: {car['vedu']}<br>
-            Type: {car['tüüp']}<br>
+            Aasta: {car['aasta']}<br>
+            Hind: {car['hind']}<br>
+            Läbisõit: {car['mileage']}<br>
+            Kütus: {car['kütus']}<br>
+            Käigukast: {car['kast']}<br>
+            Vedu: {car['vedu']}<br>
+            Keretüüp: {car['tüüp']}<br>
             <a href="{car['link']}">View listing</a><br>
             {'<img src="'+car["image_url"]+'" alt="Car image" style="width:300px;margin-top:5px;">' if 'image_url' in car else ''}
         </li>
         """
+
     html_body += "</ul>"
 
     msg.attach(MIMEText(html_body, "html"))
@@ -56,6 +57,18 @@ def send_notification_email(cars_dict):
         server.send_message(msg)
     print("Email sent successfully.")
 
+def safe_find_text(parent, by, value, default = ""):
+    try:
+        return parent.find_element(by, value).text.strip()
+    except:
+        return default
+    
+def safe_find_attr(parent, by, value, attr, default = ""):
+    try:
+        return parent.find_element(by, value).get_attribute(attr)
+    except:
+        return default
+
 # Laen olemasolevad lingid
 if os.path.exists("seen_links.json"):
     with open("seen_links.json", "r") as f:
@@ -63,13 +76,13 @@ if os.path.exists("seen_links.json"):
 else:
     seen_links = set()
 
-#setup chrome webdriver
-driver = uc.Chrome(version_main=116, headless=False) # If it starts crashing again, just download chromium
+# Setup chrome webdriver
+driver = uc.Chrome(version_main=116, headless=True) # If it starts crashing again, just download chromium
 print("Driver initialized")
 
 driver.get('https://www.auto24.ee/kasutatud/nimekiri.php?bn=2&a=100&aj=&f1=2014&g1=10000&g2=23000&l2=130000&ab%5B%5D=-1&ae=1&af=50&otsi=otsi')
 
-# web page max loading time, to make sure all elements are loaded
+# Web page max loading time, to make sure all elements are loaded
 wait = WebDriverWait(driver, 15)
 wait.until(EC.presence_of_element_located((By.CLASS_NAME, "result-row")))
 print("Cars loaded")
@@ -81,62 +94,43 @@ autod = []
 
 for auto_element in autod_elements:
     try:
-        nimi = auto_element.find_element(By.CLASS_NAME, "main").text.strip()
-        aasta = auto_element.find_element(By.CLASS_NAME, "year").text.strip()
-        
-        hind = auto_element.find_element(By.CLASS_NAME, "price").text.strip()
-        uus_hind = re.sub(r"[^\d]", "", hind)
-        try:
-            mileage = auto_element.find_element(By.CLASS_NAME, "mileage").text.strip()
-        except:
-            mileage = "Teadmata"
-
-        uus_mileage = re.sub(r"[^\d]", "", mileage)
-
-        try:
-            kütus = auto_element.find_element(By.CLASS_NAME, "fuel").text.strip()
-        except:
-            kütus = "Määramata"
-
-        kast = auto_element.find_element(By.CLASS_NAME, "transmission").text.strip()
-        tüüp = auto_element.find_element(By.CLASS_NAME, "bodytype").text.strip()
-        vedu = auto_element.find_element(By.CLASS_NAME, "drive").text.strip()   
-        link = auto_element.find_element(By.CLASS_NAME, "row-link").get_attribute("href")  
-        
+        link = safe_find_attr(auto_element, By.CLASS_NAME, "row-link", "href")
         lingid.append(link)
-        auto = {
-            "nimi": nimi,
-            "aasta": aasta,
-            "hind": uus_hind,
-            "mileage": uus_mileage,
-            "kütus": kütus,
-            "kast": kast,
-            "tüüp": tüüp,
-            "vedu": vedu,
-            "link": link
-        }
+        if link not in seen_links:
+            nimi = safe_find_text(auto_element, By.CLASS_NAME, "main")
+            aasta = safe_find_text(auto_element, By.CSS_SELECTOR, "div.extra > span.year")
+            hind = safe_find_text(auto_element, By.CSS_SELECTOR, "div.finance span.price")  
+            mileage = safe_find_text(auto_element, By.CLASS_NAME, "mileage")
+            kütus = safe_find_text(auto_element, By.CLASS_NAME, "fuel")
+            kast = safe_find_text(auto_element, By.CLASS_NAME, "transmission")
+            tüüp = safe_find_text(auto_element, By.CLASS_NAME, "bodytype")
+            vedu = safe_find_text(auto_element, By.CLASS_NAME, "drive")
+            unclean_image_url = safe_find_attr(auto_element, By.CSS_SELECTOR, "div.thumbnail span.thumb", "style")
+            clean_image_url = re.search(r'url\("(.*?)"\)', unclean_image_url).group(1) # Clean up the raw attribute into an url
 
-        autod.append(auto)
+            auto = {
+                "nimi": nimi,
+                "aasta": aasta,
+                "hind": hind,
+                "mileage": mileage,
+                "kütus": kütus,
+                "kast": kast,
+                "tüüp": tüüp,
+                "vedu": vedu,
+                "link": link,
+                "image_url": clean_image_url,
+            }
+
+            autod.append(auto)
 
     except Exception as e:
-        print("Could not extract detail:", e)
+        print("Could not extract car info:", e)
 
 # close the driver
 driver.quit()
 
-# Siia tulevad uute listingute lingid
-uued_lingid = []
-
-for link in lingid:
-    if link not in seen_links:
-        uued_lingid.append(link)
-    else:
-        break
-
-send_notification_email(" ".join(uued_lingid))
+print(autod)
+send_notification_email(autod)
 
 with open("seen_links.json", "w") as f:
     json.dump(list(lingid), f)
-
-
-print(lingid)
